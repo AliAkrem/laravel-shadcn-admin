c576e8ba1fd1cbca61e62860cf1c4fa26a04e5e2
aliakrem <barka.aliakrem@gmail.com>
2025-11-13 09:36:12 +0100

    feat: add font dynamic family

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -29,18 +29,26 @@
         />
         <meta name="title" content="{{config('app.name', 'Laravel')}}" />
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        
         <meta
             name="description"
             content="Admin Dashboard UI built with Laravel, Shadcn and Vite."
         />
 
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Manrope:wght@200..800&display=swap"
+        rel="stylesheet"
+        />
+
+        <meta name="theme-color" content="#fff" />
+        
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body>
         @inertia
     </body>
 </html>
